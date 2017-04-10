3af76d2b195d835cddd32cb52e16661fb80d7fba
unknown <>
2017-04-10 17:18:19 +0800

    create:TaskController.php{method_Submit}

--- app/Base.php
+++ app/Base.php
@@ -43,4 +43,21 @@ class Base extends Model {
 			]);
 		}
 	}
+	
+	// 通过地理坐标计算距离
+	public static function distanceBetween($fP1Lat, $fP1Lon, $fP2Lat, $fP2Lon) {
+		$fEARTH_RADIUS = 6378137;
+		//角度换算成弧度
+		$fRadLon1 = deg2rad($fP1Lon);
+		$fRadLon2 = deg2rad($fP2Lon);
+		$fRadLat1 = deg2rad($fP1Lat);
+		$fRadLat2 = deg2rad($fP2Lat);
+		//计算经纬度的差值
+		$fD1 = abs($fRadLat1 - $fRadLat2);
+		$fD2 = abs($fRadLon1 - $fRadLon2);
+		//距离计算
+		$fP = pow(sin($fD1/2), 2) +
+		cos($fRadLat1) * cos($fRadLat2) * pow(sin($fD2/2), 2);
+		return intval($fEARTH_RADIUS * 2 * asin(sqrt($fP)) + 0.5);
+	}
 }
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -129,11 +129,11 @@ class TaskController extends Controller {
 	
 	/**
 	 * 提示任务
-	 * 
-	 * @param unknown $lat 
-	 * @param unknown $lng
-	 * @param unknown $id
-	 * @param string $email
+	 *
+	 * @param unknown $lat        	
+	 * @param unknown $lng        	
+	 * @param unknown $id        	
+	 * @param string $email        	
 	 * @return \Illuminate\Http\JsonResponse|unknown
 	 */
 	public function Prompt($latitude = -1, $longitude = -1, $id = -1, $email = '') {
@@ -153,7 +153,7 @@ class TaskController extends Controller {
 		$baidumap = new BaiduMap ();
 		$coordinate = $baidumap->exchangeCoordinate ( $latitude, $longitude );
 		
-		// 获得任务目的信息
+		// 获得任务目地信息
 		$task_information = explode ( ':', Redis::get ( $id . ':' . $email . ':taskinformation' ) );
 		
 		// 生成提示视图并展示
@@ -165,56 +165,80 @@ class TaskController extends Controller {
 		return view ( 'prompt' )->with ( 'location', $location );
 	}
 	
+	/**
+	 * 任务验证
+	 * 
+	 * @param Request $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
 	public function Submit(Request $request) {
 		$status = '200';
 		if (Auth::check ()) {
 			// 二次判断是否由POST方法传入参数
 			if (! $request->isMethod ( 'POST' )) {
 				$status = '600';
-				return response ()->json ( [
-						'status' => $status
+				return response ()->json ( [ 
+						'status' => $status 
 				] );
 			}
-				
+			
 			// 获得用户基本信息
 			$user = Auth::user ();
-				
-			// 判断是否还有未完成任务
-			$task_status_value = Redis::get ( $user ['id'] . ':' . $user ['email'] . ':taskstatus' );
-			if ($task_status_value == '1') {
-				$status = '901';
-				return response ()->json ( [
-						'status' => $status
-				] );
-			}
-				
+			
 			// 获得POST参数
-			$lat = $request->input ( "lat" );
-			$lng = $request->input ( "lng" );
-			$name = $request->input ( "name" );
-			$address = $request->input ( "address" );
-			$uid = $request->input ( "uid" );
+			$latitude = $request->input ( "latitude" );
+			$longitude = $request->input ( "longitude" );
 			$_tokenpasswd = $request->input ( "_tokenpasswd" );
-				
+			
 			// 进行登陆方式验证，防止暴力登陆
 			Base::tokenPasswdVerificate ( $request, $_tokenpasswd );
+			
+			// 判断是否还有未完成任务
+			$task_status_value = Redis::get ( $user ['id'] . ':' . $user ['email'] . ':taskstatus' );
+			if ($task_status_value == '0') {
+				$status = '900';
+				return response ()->json ( [ 
+						'status' => $status 
+				] );
+			}
+			
+			// 处理初始坐标
+			$baidumap = new BaiduMap ();
+			$coordinate = $baidumap->exchangeCoordinate ( $latitude, $longitude );
+			
+			// 获得任务目地信息
+			$task_information = explode ( ':', Redis::get ( $id . ':' . $email . ':taskinformation' ) );
+			
+			// 计算距离
+			$distance = Base::distanceBetween ( $coordinate ['latitude'], $coordinate ['longitude'], $task_information [0], $task_information [1] );
+			
+			//返回信息
+			if ($distance < 60) {
+				// 向Redis写入任务
+				$task_status_key = $user ['id'] . ':' . $user ['email'] . ':taskstatus';
+				$task_status_value = '0';
+				$task_information_key = $user ['id'] . ':' . $user ['email'] . ':taskinformation';
+				Redis::set ( $task_status_key, $task_status_value );
 				
-			// 向Redis写入任务
-			$task_status_key = $user ['id'] . ':' . $user ['email'] . ':taskstatus';
-			$task_status_value = '1';
-			$task_information_key = $user ['id'] . ':' . $user ['email'] . ':taskinformation';
-			$task_information_value = $lat . ':' . $lng . ':' . $name . ':' . $address . ':' . $uid;
-			Redis::set ( $task_status_key, $task_status_value );
-			Redis::set ( $task_information_key, $task_information_value );
-				
-			return response ()->json ( [
-					'status' => $status
-			] );
+				// 删除任务地点
+				Redis::delete ( $task_information_key );
+				return response ()->json ( [
+						'status' => $status,
+						'message' => '恭喜你, 任务完成',
+						'message_status' => '200'
+				] );
+			} else {
+				return response ()->json ( [
+						'status' => $status,
+						'message' => '距离目的地还有' . $distance . '米',
+						'message_status' => '300'
+				] );
+			}
 		} else {
 			// 未登录
 			$status = '400';
-			return response ()->json ( [
-					'status' => $status
+			return response ()->json ( [ 
+					'status' => $status 
 			] );
 		}
 	}
